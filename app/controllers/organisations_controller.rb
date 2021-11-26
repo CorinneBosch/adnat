37e0ee119a5cda22f3b4a320c70f7ac7ed0bda10
Corinne <corinne.boesch@gmail.com>
2021-11-26 22:20:41 +0000

    can create new organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -2,4 +2,29 @@ class OrganisationsController < ApplicationController
   def index
     @organisations = Organisation.all.order(created_at: :desc)
   end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def new
+    @organisation = Organisation.new 
+  end
+
+  def create
+    @organisation = Organisation.create(org_params)
+    if @organisation.save
+      redirect_to @organisation #, notice: 'Organisation created successfully!'
+    else
+      render :new #, notice: 'Oops something went wront.. Please try again!'
+    end
+  end
+
+  private
+
+  def org_params
+    params.require(:organisation).permit(:name, :hourly_rate)
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'organisations#index'
   
-  # resources :organisations
-  get '/organisations', to: 'organisations#index'
+  resources :organisations
+  # get 'organisations', to: 'organisations#index'
+  # get 'organisations/:id', to: 'organisations#show'
+
+  # get 'organisations', to: 'organisations#new'
+  # post 'organisations/:id', to: 'organisations#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
