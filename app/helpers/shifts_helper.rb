7a35008118999f574de3173a5e9525787a28374d
Corinne <corinne.boesch@gmail.com>
2021-11-27 19:48:47 +0000

    can calculate total hours worked and shift cost

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,2 +1,15 @@
+require 'time'
+
 module ShiftsHelper
+  def total_hours_worked(shift_start, shift_finish, break_min)
+    finish = Time.parse(shift_finish.strftime('%H:%M'))
+
+    min_worked = ((finish - shift_start) / 60) - break_min
+    round_to_quarter(min_worked / 60)
+  end
+
+  # private
+  def round_to_quarter(hours)
+    (hours * 4).round / 4.0
+  end
 end
