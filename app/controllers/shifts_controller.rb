5279bb6de3e9dbf4469b406d108d3eb80ddf2f5b
Corinne <corinne.boesch@gmail.com>
2021-11-27 21:50:47 +0000

    can delete and edit shifts

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,20 +1,43 @@
 class ShiftsController < ApplicationController
-  before_action :join_params, only: [:create]
+  before_action :join_params, only: [:create, :edit]
+  before_action :find_organisation
+  before_action :find_shift, only: [:destroy, :edit]
+
 
   def index
-    @organisation = Organisation.find(params[:organisation_id])
+    # @organisation = Organisation.find(params[:organisation_id])
     @org_shifts = @organisation.shifts.all.order(created_at: :desc)
   end
 
   def create 
-    @organisation = Organisation.find(params[:organisation_id])
+    # @organisation = Organisation.find(params[:organisation_id])
     @shift = @organisation.shifts.create(shift_params)
-    redirect_to organisation_shifts_path(@organisation), notice: 'Shift added!'
+    if @shift.save
+      redirect_to organisation_shifts_path(@organisation), notice: 'Shift added!'
+    else
+      render :index, notice: 'Oops something went wront.. Please try again!'
+    end
+  end
+
+  def edit
+    # @organisation = Organisation.find(params[:organisation_id])
+    # @shift = @organisation.shifts.find(params[:id])
+  end
+
+  def update
+    # @organisation = Organisation.find(params[:organisation_id])
+    @shift = @organisation.shifts.find(params[:id])
+
+    if @shift.update(shift_params)
+      redirect_to organisation_shifts_path(@organisation), notice: 'Shift updated successfully!'
+    else
+      render :index, notice: 'Oops something went wront.. Please try again!'
+    end
   end
 
   def destroy
-    @organisation = Organisation.find(params[:organisation_id])
-    @shift = @organisation.shifts.find(params[:id])
+    # @organisation = Organisation.find(params[:organisation_id])
+    # @shift = @organisation.shifts.find(params[:id])
     @shift.destroy
     redirect_to organisation_shifts_path(@organisation), notice: 'Shift deleted!'
   end
@@ -36,5 +59,14 @@ class ShiftsController < ApplicationController
     # else
     params[:shift][:shift_start] = Time.parse("#{shift_start_date} #{shift_start_time}")
     # end
+  end
+
+  def find_organisation
+    @organisation = Organisation.find(params[:organisation_id])
+  end
+
+  def find_shift
+    # @shift = @organisation.shifts.find(params[:id])
+    @shift = Shift.find(params[:id])
   end
 end
