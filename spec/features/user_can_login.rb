60d246dbf154282903c45685bcb1a36df972fc05
Corinne <corinne.boesch@gmail.com>
2021-12-11 13:13:54 +0000

    user can login feature test

---
+++ spec/features/user_can_login.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature 'Log in', type: :feature do
+
+  before :each do
+    @user = create(:user)
+  end
+
+  scenario 'Existing user can log in with correct email & password' do
+    visit user_session_url
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+    click_button 'Log in'
+    expect(page).to have_current_path(organisations_url)
+  end
+
+  scenario 'Unable to login with invalid details' do
+    visit user_session_url
+    fill_in :email, with: 'wrongpassword'
+    fill_in :password, with: 'wrong@email.com'
+    click_button 'Log in'
+    expect(page).to have_current_path(user_session_url)
+  end
+end
